2fc946b44cde6d517cad45d78f5b7ff0f980dfed
KanekoK <b1r2d3q4@gmail.com>
2016-04-17 11:42:04 +0900

    更新！

--- yakudoshi.php
+++ yakudoshi.php
@@ -4,17 +4,17 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title>結果</title>
-	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/yakudoshi.css">
 </head>
 <body>
 	<?php
 	$kotoshi = $_POST["seireki"];
 
 	$arr_eto = [
-	"猿", "鳥", "犬",
-	"猪", "鼠", "牛",
-	"虎", "兎", "竜",
-	"蛇", "馬", "羊",
+	"申", "酉", "戌",
+	"亥", "子", "丑",
+	"寅", "卯", "辰",
+	"巳", "午", "未",
 	];
 
 
@@ -56,107 +56,115 @@
 
 
 	?>
-	<main>
-		<div class="man">
-			<h1>男性厄年</h1>
-			<h2><?php echo $_POST["seireki"]."年"; ?></h2>
-
-			<table border=1>
-				<tr>
-					<th></th>
-					<th>前厄</th>
-					<th>本厄</th>
-					<th>後厄</th>
-				</tr>
-				<tr>
-					<th>25歳</th>
-					<th></th>
-					<th>
-						<?php echo $man_yaku1."年";?><br>
-						<?php echo $man_yakudoshi1."どし"; ?>
-					</th>
-					<th></th>
-				</tr>
-				<tr>
-					<th>42歳</th>
-					<th>
-						<?php echo $man_maeyaku2."年"; ?><br>
-						<?php echo $man_maeyakudoshi2."どし"; ?>
-					</th>
-					<th>
-						<?php echo $man_yaku2."年";?><br>
-						<?php echo $man_yakudoshi2."どし"; ?>
-					</th>
-					<th>
-						<?php echo $man_atoyaku2."年";?><br>
-						<?php echo $man_atoyakudoshi2."どし"; ?>
-					</th>
-				</tr>
-				<tr>
-					<th>61歳</th>
-					<th></th>
-					<th>
-						<?php echo $man_yaku3."年";?><br>
-						<?php echo $man_yakudoshi3."どし"; ?>
-					</th>
-					<th></th>
-				</tr>
-			</table>
-		</div> <!-- .man -->
-		<div class="woman">
-			<h1>女性厄年</h1>
-
-			<table border=1>
-				<tr>
-					<th></th>
-					<th>前厄</th>
-					<th>本厄</th>
-					<th>後厄</th>
-				</tr>
-				<tr>
-					<th>19歳</th>
-					<th></th>
-					<th>
-						<?php echo $woman_yaku1."年";?><br>
-						<?php echo $woman_yakudoshi1."どし"; ?>
-					</th>
-					<th></th>
-				</tr>
-				<tr>
-					<th>33歳</th>
-					<th>
-						<?php echo $woman_maeyaku2."年"; ?><br>
-						<?php echo $woman_maeyakudoshi2."どし"; ?>
-					</th>
-					<th>
-						<?php echo $woman_yaku2."年";?><br>
-						<?php echo $woman_yakudoshi2."どし"; ?>
-					</th>
-					<th>
-						<?php echo $woman_atoyaku2."年";?><br>
-						<?php echo $woman_atoyakudoshi2."どし"; ?>
-					</th>
-				</tr>
-				<tr>
-					<th>37歳</th>
-					<th></th>
-					<th>
-						<?php echo $woman_yaku3."年";?><br>
-						<?php echo $woman_yakudoshi3."どし"; ?>
-					</th>
-					<th></th>
-				</tr>
-				<tr>
-					<th>61歳</th>
-					<th></th>
-					<th>
-						<?php echo $woman_yaku4."年";?><br>
-						<?php echo $woman_yakudoshi4."どし"; ?>
-					</th>
-					<th></th>
-				</tr>
-			</table>
+	<header>
+		<div id="header">
+			<h1><?php echo $_POST["seireki"]."年"; ?></h1>
 		</div>
+	</header>
+	<main>
+		<div id="main">
+			<div class="common clearfix">
+				<div class="man">
+					<h1>男性厄年</h1>
+					<h2>年齢は数え年</h2>
+					<table border=1>
+						<tr>
+							<th></th>
+							<th>前厄</th>
+							<th>本厄</th>
+							<th>後厄</th>
+						</tr>
+						<tr>
+							<th>25歳</th>
+							<th></th>
+							<th>
+								<?php echo $man_yaku1."年";?><br>
+								<?php echo $man_yakudoshi1; ?>
+							</th>
+							<th></th>
+						</tr>
+						<tr>
+							<th>42歳</th>
+							<th>
+								<?php echo $man_maeyaku2."年"; ?><br>
+								<?php echo $man_maeyakudoshi2; ?>
+							</th>
+							<th>
+								<?php echo $man_yaku2."年";?><br>
+								<?php echo $man_yakudoshi2; ?>
+							</th>
+							<th>
+								<?php echo $man_atoyaku2."年";?><br>
+								<?php echo $man_atoyakudoshi2; ?>
+							</th>
+						</tr>
+						<tr>
+							<th>61歳</th>
+							<th></th>
+							<th>
+								<?php echo $man_yaku3."年";?><br>
+								<?php echo $man_yakudoshi3; ?>
+							</th>
+							<th></th>
+						</tr>
+					</table>
+				</div> <!-- .man -->
+				<div class="woman">
+					<h1>女性厄年</h1>
+					<h2>年齢は数え年</h2>
+					<table border=1>
+						<tr>
+							<th></th>
+							<th>前厄</th>
+							<th>本厄</th>
+							<th>後厄</th>
+						</tr>
+						<tr>
+							<th>19歳</th>
+							<th></th>
+							<th>
+								<?php echo $woman_yaku1."年";?><br>
+								<?php echo $woman_yakudoshi1; ?>
+							</th>
+							<th></th>
+						</tr>
+						<tr>
+							<th>33歳</th>
+							<th>
+								<?php echo $woman_maeyaku2."年"; ?><br>
+								<?php echo $woman_maeyakudoshi2; ?>
+							</th>
+							<th>
+								<?php echo $woman_yaku2."年";?><br>
+								<?php echo $woman_yakudoshi2; ?>
+							</th>
+							<th>
+								<?php echo $woman_atoyaku2."年";?><br>
+								<?php echo $woman_atoyakudoshi2; ?>
+							</th>
+						</tr>
+						<tr>
+							<th>37歳</th>
+							<th></th>
+							<th>
+								<?php echo $woman_yaku3."年";?><br>
+								<?php echo $woman_yakudoshi3; ?>
+							</th>
+							<th></th>
+						</tr>
+						<tr>
+							<th>61歳</th>
+							<th></th>
+							<th>
+								<?php echo $woman_yaku4."年";?><br>
+								<?php echo $woman_yakudoshi4; ?>
+							</th>
+							<th></th>
+						</tr>
+					</table>
+				</div>
+			</div>
+		</div> <!-- #main -->
 	</main>
 </body>
 </html>
